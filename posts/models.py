39a443db389667cd3a651ab891fce720f04c5c4d
VyshnaviMulakalapalli <vm1096@usnh.edu>
2025-03-10 12:08:01 -0400

    add model for followers

--- posts/models.py
+++ posts/models.py
@@ -27,3 +27,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user.username} commented on {self.post.id}"
+
+class Follow(models.Model):
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="followers")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("follower", "following")
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.following.username}"
