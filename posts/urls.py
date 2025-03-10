ec981571c7b1fe6cdf7b00856f4e122129cf1cb5
VyshnaviMulakalapalli <vm1096@usnh.edu>
2025-03-10 12:12:27 -0400

    add urls for followers and newsfeed

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import PostListCreateView, PostDetailView, LikePostView, CommentListView
+from .views import PostListCreateView, PostDetailView, LikePostView, CommentListView, FollowUserView, FollowerListView, NewsfeedView, FollowingListView
 
 urlpatterns = [
     path("", PostListCreateView.as_view(), name="post-list-create"),
     path("<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("<int:post_id>/like/", LikePostView.as_view(), name="like-post"),
     path("<int:post_id>/comments/", CommentListView.as_view(), name="comment-list-create"),
+    path("follow/<int:user_id>/", FollowUserView.as_view(), name="follow-user"),
+    path("<int:user_id>/followers/", FollowerListView.as_view(), name="follower-list"),
+    path("<int:user_id>/following/", FollowingListView.as_view(), name="following-list"),
+    path("newsfeed/", NewsfeedView.as_view(), name="newsfeed"),
 ]
